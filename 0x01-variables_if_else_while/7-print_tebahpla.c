be3deadeb7d97523cd0855d4c4a532790d48e55f
Joseph <joyadongha@gmail.com>
2022-10-16 12:51:19 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i = 'z';
+while (i >= 'a')
+{
+putchar(i);
+i--;
+}
+putchar('\n');
+return (0);
+}
