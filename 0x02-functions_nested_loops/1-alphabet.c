c42112a9865dceb13d98e49d3aeb9a68b24c9bb7
Joseph <joyadongha@gmail.com>
2022-10-19 05:33:39 -0700

    Updating program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
 #include "main.h"
-#include "_putchar.c"
+void print_alphabet(void);
 /**
  * main - entry point
  * Description: Prints _putchar with _putchar
  * Return: Always 0 (Success)
 */
+int main(void)
+{
+print_alphabet();
+_putchar('\n');
+return (0);
+}
 void print_alphabet(void)
 {
 char c = 'a';
@@ -13,11 +19,4 @@ while (c <= 'z')
 _putchar(c);
 c++;
 }
-return (0);
-}
-int main(void)
-{
-print_alphabet();
-_putchar('\n');
-return (0);
 }
