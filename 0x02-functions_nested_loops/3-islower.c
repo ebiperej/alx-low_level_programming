d0ad6292edb0ec0e9648bf6e7033ec259385153f
Joseph <joyadongha@gmail.com>
2022-10-19 08:53:47 -0700

    updates added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,13 @@ int _islower(int c);
 /**
  * _islower - Checks alphabets if it is lowercase
  * @c: If lowercase alphabet
- * Return: Always 0 (Success)
+ * Return: 1 if lowercase and 0 if otherwise
 */
 int _islower(int c)
-{
-for (c = 0; c <= 126; c++)
 {
 if ((c >= 97) && (c <= 122))
 {
 return (1);
 }
 return (0);
-}
 }
