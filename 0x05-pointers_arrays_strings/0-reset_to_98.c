73bdfa6aa737f0ad2579071b1885ad39a6cf3fa6
Joseph <joyadongha@gmail.com>
2022-10-24 12:57:50 -0700

    adding files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * reset_to_98 - updates the value of a variable with a pointer
+  * Return: success
+  * @*n: pointer to variable
+*/
+void reset_to_98(int *n){
+int n;
+int *n = &n;
+*n = 98;
+return (*n);
+}
