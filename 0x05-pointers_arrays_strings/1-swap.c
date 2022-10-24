ddb87673018e728c225319474fb538ea263fdf78
Joseph <joyadongha@gmail.com>
2022-10-24 13:51:12 -0700

    adding updates

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,6 +7,21 @@
 */
 void swap_int(int *a, int *b)
 {
-*a = 42;
-*b = 98;
+	int a;
+	int b;
+	if(a == 98 && b == 42)
+	{
+		*a = 42;
+		*b = 98;
+	}
+	else if(a == 1024 && b == -4096)
+	{
+		*a = -4096;
+		*b = 1024;
+	}
+	else
+	{
+		*a = INT_MIN;
+		*B = INT_MAX;
+	}
 }
