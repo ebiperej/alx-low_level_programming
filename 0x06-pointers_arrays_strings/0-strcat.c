dfd0974baeb2a8bd167b974852ce09dd667ec561
Joseph <joyadongha@gmail.com>
2022-10-26 09:46:40 -0700

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,20 @@
 /**
   * *_strcat - concatenates two strings
   * Return: pointer to resulting string
-  * @dest: destination string
+  * @dest: pointer to destination string
   * @src: source string
   */
-char *_strcat(char *dest, char *src)
-{
-	char *ptr;
-	*ptr = "*dest + *src\n";
-	return (*ptr);
+  char *_strcat(char *dest, char *src)
+  {
+	  int i;
+	  int j;
+
+	  i = 0;
+	  for (j = 0; dest[j] != '\0'; j++);
+	  while (src[i] != '\0')
+	  {
+		  dest[j + i] = src[i];
+		  i++;
+	  }
+	  return (dest);
 }
