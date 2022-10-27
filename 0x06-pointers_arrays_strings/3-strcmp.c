6203fbfad76c93ee810bdfeaf8408c9dfc39e58e
Joseph <joyadongha@gmail.com>
2022-10-27 11:00:09 -0700

    fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,22 +7,10 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		j = 0;
-	while (*(s2 + j) != '\0')
-		j++;
-	if (*(s1 + i) == *(s2 + j))
-	{
-		return (0);
-	}
-	else if (*(s1 + i) < *(s2 + j))
-	{
-	return (-15);
-	}
-	else
-	{
-	return (15);
+	for (i = 0; *(s1 + i) != '\0' && *(s2 + i)
+		!= '\0' && *(s1 + i) == *(s2 + i); i++)
+	return (*(s1 + i) - *(s2 + i));
 	}
 }
