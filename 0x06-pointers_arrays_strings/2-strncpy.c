2b13a82a4b375893de5b849ab193709570425f3c
Joseph <joyadongha@gmail.com>
2022-10-27 10:01:00 -0700

    fixed errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	i = 0;
+	while (i < n && *(src + i))
+		{
 		*(dest + i) = *(src + i);
-	for (i = 0; i < n; i++)
+		i++;
+		}
+		while (i < n)
+		{
 		*(dest + i) = '\0';
-	return (dest);
+		i++;
+		}
+return (dest);
 }
