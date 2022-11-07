b073b3b7cf6e472f988fa7c10bb93fe1af1b936f
Joseph <joyadongha@gmail.com>
2022-11-07 09:43:25 +0000

    fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * _strchr - locates a character in a string
  * @s: the string to check
@@ -11,11 +12,17 @@ char *_strchr(char *s, char c)
 	while (*s)
 	{
 		if (*s != c)
+		{
 			s++;
+		}
 		else
+		{
 			return (s);
+		}
 	}
 	if (c == '\0')
+	{
 		return (s);
+	}
 	return (NULL);
 }
