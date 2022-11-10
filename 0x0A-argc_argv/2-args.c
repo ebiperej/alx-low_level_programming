693f2b2bf18f43cce70e7dbcc9314c10315fd5a3
Joseph <joyadongha@gmail.com>
2022-11-10 21:15:43 +0000

    corrected

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,9 +12,14 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i <= argc; i++)
+	if (argc > 1)
 	{
-		printf("%s\n", argv[i]);
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
 	}
+	else
+		printf("No arguments provided");
 	return (0);
 }
