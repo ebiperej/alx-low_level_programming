2a61b58fbb75269b3b3865290d3ecd37acdacd77
Joseph <joyadongha@gmail.com>
2022-11-19 19:26:14 +0000

    adding structure for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - structure for a dog
+  * @name: name of dog
+  * @age: dog age
+  * @owner: dog owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
